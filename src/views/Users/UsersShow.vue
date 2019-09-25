1dc11a3261e62301e477518c5413781db1a82426
Benjamin Foster <bfoster@marlboro.edu>
2019-09-25 09:07:13 -0400

    adds styles

--- src/views/Users/UsersShow.vue
+++ src/views/Users/UsersShow.vue
@@ -12,33 +12,21 @@
               <h3 style="text-align:center;margin-bottom: 20px;">Recent Posts</h3>
               <div v-for="post in posts" :key="post.id">
                 
-              <article>
+              <article class="post">
                 <div class="news-container">
-                  <h1 class="news-title"><router-link v-bind:to="'../posts/' + post.id"><span v-if="isMostHated(post)" id="hated">Most Hated:<br /></span>{{ post.title }}</router-link></h1>
-                  <span class="news-date">{{ post.last_edited }}</span>
-                  <div class="news-entry">
-                    <p>{{ shortener(post.text) }}</p>
-                  </div><!-- .news-entry -->
-                  <div class="news-footer">
-                   
-                      <i class="fa fa-comments-o"></i> <router-link v-bind:to="'/posts/' + post.id"> Comments {{ post.comments.length }} </router-link>
-
-                      <span v-if="isLoggedIn()">
-
-                        <button v-bind:class="{ hide: !(current_user.downvoted_post_ids[post.id] == true) }" v-on:click="removeDownvote(post)" style="margin-left: 10px; font-size:40px; padding: 10px;" class="btn btn-danger">↓ {{ post.total_downvotes }}</button>
-
-                        <button v-bind:class="{ hide: current_user.downvoted_post_ids[post.id] == true }" v-on:click="downvote(post)" style="margin-left: 10px; font-size:40px; padding: 10px;" class="btn btn-primary">↓ {{ post.total_downvotes }}</button>
-
-                      </span>
-
-                      <span v-else>
-
-                        <button style="margin-left: 10px; font-size:40px; padding: 10px;" class="btn btn-primary">↓ {{ post.total_downvotes }}</button>
-                        
-                      </span>
-                    
-                  </div><!-- .news-footer -->
-                </div><!-- .news-container -->
+                  <h2 class="news-title"><router-link v-bind:to="'../posts/' + post.id"><span v-if="isMostHated(post)" id="hated"><strong>Most Hated </strong></span>{{ post.title }}</router-link></h2>
+                  <span class="news-category"><router-link v-bind:to="'users/' + user.id">{{ user.name }}</router-link></span>
+                  <span class="news-date">{{ relativeDate(post.last_edited) }}</span>
+                      <div class="news-entry">{{post.text.substring(0,470)}}<span style="color:gray;">{{post.text.substring(470,479)}}</span><span style="color:silver;">{{post.text.substring(479,490)}}</span><span style="color:lightgray;">{{post.text.substring(490,500)}}</span>
+                        <span style="white-space:nowrap;"><span style="font-size:14px;"><i class="fa fa-comments-o"></i><router-link v-bind:to="'posts/' + post.id"> Comments {{ post.comments.length }}</router-link></span>
+                        <span class="pointer lil-fade" style="display:inline-block;">
+                        <span v-if="isLoggedIn()">
+                            <span v-bind:class="{ hide: !(current_user.downvoted_post_ids[post.id] == true) }" v-on:click="removeDownvote(post)" style="font-size:25px;font-family: 'VT323', monospace;padding:5px;color:red;">↓{{ post.total_downvotes }}</span>
+                            <span v-bind:class="{ hide: current_user.downvoted_post_ids[post.id] == true }" style="font-size:25px;font-family: 'VT323', monospace;padding:5px;color:navy;" v-on:click="downvote(post)">↓{{ post.total_downvotes }}</span>
+                          </span>
+                          <a v-else><span style="font-size:25px;font-family: 'VT323', monospace;padding:5px;color:navy;">↓{{ post.total_downvotes }}</span></a></span>
+                        </span>
+                </div></div><!-- .news-container -->
               </article>
       </div>
       </div>
@@ -73,12 +61,153 @@
 #hated {
       font-family: 'Titillium Web', sans-serif;
       font-size: 20px;
-      font-weight: 200;
-    }     
+    }    
+    .lil-fade {
+  -webkit-animation: fadein 10s infinite ease-in-out; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: fadein 10s infinite ease-in-out; /* Firefox < 16 */
+        -ms-animation: fadein 10s infinite ease-in-out; /* Internet Explorer */
+         -o-animation: fadein 10s infinite ease-in-out; /* Opera < 12.1 */
+            animation: fadein 10s infinite ease-in-out;
+}
+.blinky {
+  display:inline-block;
+  -webkit-animation: blink 8s infinite ease-in-out; /* Safari, Chrome and Opera > 12.1 */
+       -moz-animation: blink 8s infinite ease-in-out; /* Firefox < 16 */
+        -ms-animation: blink 8s infinite ease-in-out; /* Internet Explorer */
+         -o-animation: blink 8s infinite ease-in-out; /* Opera < 12.1 */
+            animation: blink 8s infinite ease-in-out;
+} 
+
+@keyframes blink {
+      40%, 100% {
+    opacity: 1;
+  }
+  0% {
+    opacity: 0;
+  }
+  30% {
+    opacity: 1;
+  }
+  }
+/* Firefox < 16 */
+@-moz-keyframes blink {
+   40%, 100% {
+    opacity: 1;
+  }
+  0% {
+    opacity: 0;
+  }
+  30% {
+    opacity: 1;
+  }
+}
+
+/* Safari, Chrome and Opera > 12.1 */
+@-webkit-keyframes blink {
+   40%, 100% {
+    opacity: 1;
+  }
+  0% {
+    opacity: 0;
+  }
+  30% {
+    opacity: 1;
+  }
+}
+
+/* Internet Explorer */
+@-ms-keyframes blink {
+  40%, 100% {
+    opacity: 1;
+  }
+  0% {
+    opacity: 0;
+  }
+  30% {
+    opacity: 1;
+  }
+}
+
+/* Opera < 12.1 */
+@-o-keyframes blink {
+   40%, 100% {
+    opacity: 1;
+  }
+  0% {
+    opacity: 0;
+  }
+  30% {
+    opacity: 1;
+  }
+}
+
+ @keyframes fadein {
+      40%, 100% {
+    opacity: 1;
+  }
+  0% {
+    opacity: 0;
+  }
+  30% {
+    opacity: 1;
+  }
+  }
+/* Firefox < 16 */
+@-moz-keyframes blink {
+   40%, 100% {
+    opacity: 1;
+  }
+  0% {
+    opacity: 0;
+  }
+  30% {
+    opacity: 1;
+  }
+}
+
+* Safari, Chrome and Opera > 12.1 */
+@-webkit-keyframes fadein {
+   40%, 100% {
+    opacity: 1;
+  }
+  0% {
+    opacity: 0;
+  }
+  30% {
+    opacity: 1;
+  }
+}
+
+/* Internet Explorer */
+@-ms-keyframes fadein {
+  40%, 100% {
+    opacity: 1;
+  }
+  0% {
+    opacity: 0;
+  }
+  30% {
+    opacity: 1;
+  }
+}
+
+/* Opera < 12.1 */
+@-o-keyframes fadein {
+   40%, 100% {
+    opacity: 1;
+  }
+  0% {
+    opacity: 0;
+  }
+  30% {
+    opacity: 1;
+  }
+}
 </style>
 
 <script>
 import axios from "axios";
+import moment from 'moment';
 export default {
   data: function() {
     return {
@@ -140,6 +269,9 @@ export default {
       } else {
         return false;
       }
+    },
+    relativeDate: function(date) {
+      return moment(date).fromNow();
     }	
     }	
   }
